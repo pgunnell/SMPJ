1b8bd85563a624ed1e1e436e76a371d496daa68b
gflouris <flourisj@gmail.com>
2015-06-15 11:38:11 +0300

    calo jets

--- AnalysisFW/interface/QCDEvent.h
+++ AnalysisFW/interface/QCDEvent.h
@@ -5,7 +5,7 @@
 #define QCDEvent_h
 #include "SMPJ/AnalysisFW/interface/QCDJet.h"
 #include "SMPJ/AnalysisFW/interface/QCDMET.h"
-//#include "SMPJ/AnalysisFW/interface/QCDCaloJet.h"
+#include "SMPJ/AnalysisFW/interface/QCDCaloJet.h"
 #include "SMPJ/AnalysisFW/interface/QCDPFJet.h"
 #include "SMPJ/AnalysisFW/interface/QCDEventHdr.h"
 #include "DataFormats/JetReco/interface/Jet.h"
@@ -20,10 +20,10 @@ class QCDEvent
       //------------ Destructor -------------------------------
       ~QCDEvent();
       //------------ Set methods ------------------------------
-      //void setCaloMET(const QCDMET& fCaloMET)                     {CaloMet_ = fCaloMET;}
+      void setCaloMET(const QCDMET& fCaloMET)                     {CaloMet_ = fCaloMET;}
       void setPFMET(const QCDMET& fPFMET)                         {PFMet_ = fPFMET;}
       void setEvtHdr(const QCDEventHdr& fEvtHdr)                  {EvtHdr_ = fEvtHdr;}
-      //void setCaloJets(const std::vector<QCDCaloJet>& fCaloJets);
+      void setCaloJets(const std::vector<QCDCaloJet>& fCaloJets);
       void setPFJets(const std::vector<QCDPFJet>& fPFJets);
       void setPFJetsCHS(const std::vector<QCDPFJet>& fPFJetsCHS);
       //void setFatJets(const std::vector<QCDJet>& fFatJets);
@@ -39,7 +39,7 @@ class QCDEvent
       unsigned int nPFJets()                           const {return PFJets_.size();}
       unsigned int nPFJetsCHS()                        const {return PFJetsCHS_.size();}
       //unsigned int nFatJets()                          const {return FatJets_.size();}
-      //unsigned int nCaloJets()                         const {return CaloJets_.size();}
+      unsigned int nCaloJets()                         const {return CaloJets_.size();}
       unsigned int nGenJets()                          const {return GenJets_.size();}
       int nGoodJets(int unc, int id, float ymax, float ptmin, std::vector<QCDJet> jets);
       int fired(int i)                                 const {return TriggerDecision_[i];}
@@ -47,17 +47,17 @@ class QCDEvent
       int preHLT(int i)                                const {return HLTPrescale_[i];}
       float pfmjj();
 
-      //float calomjj();
+      float calomjj();
       float genmjj();
       float pfchsmjjcor(int unc);
       float pfchsmjjcor(int unc,int src);
       float pfmjjcor(int unc);
       float pfmjjcor(int unc,int src);
       //float fatmjjcor(int unc);
-      //float calomjjcor(int unc);
+      float calomjjcor(int unc);
       float pfmjjgen();
-      //float calomjjgen();
-      //const QCDMET&        calomet()                   const {return CaloMet_;}
+      float calomjjgen();
+      const QCDMET&        calomet()                   const {return CaloMet_;}
       const QCDMET&        pfmet()                     const {return PFMet_;}
       const LorentzVector& hltobj(int itrig, int iobj) const {return (HLTObj_[itrig])[iobj];}
       const LorentzVector& l1obj(int itrig, int iobj)  const {return (L1Obj_[itrig])[iobj];}
@@ -65,14 +65,14 @@ class QCDEvent
       const QCDPFJet&      pfjet(int i)                const {return PFJets_[i];}
       const QCDPFJet&      pfjetchs(int i)             const {return PFJetsCHS_[i];}
       //const QCDJet&        fatjet(int i)               const {return FatJets_[i];}
-      //const QCDCaloJet&    calojet(int i)              const {return CaloJets_[i];}
+      const QCDCaloJet&    calojet(int i)              const {return CaloJets_[i];}
       const QCDEventHdr&   evtHdr()                    const {return EvtHdr_;}
 
     private:
       //---- event header (contains all the event info) --------------
       QCDEventHdr                              EvtHdr_;
       //---- CALO met object -----------------------------------------
-      //QCDMET                                   CaloMet_;
+      QCDMET                                   CaloMet_;
       //---- PF met object -------------------------------------------
       QCDMET                                   PFMet_;
       //---- trigger decision vector ---------------------------------
@@ -88,7 +88,7 @@ class QCDEvent
       //---- Genjets -------------------------------------------------
       std::vector<LorentzVector>               GenJets_;
       //---- CaloJets ------------------------------------------------
-      //std::vector<QCDCaloJet>                  CaloJets_;
+      std::vector<QCDCaloJet>                  CaloJets_;
       //---- PFJets --------------------------------------------------
       std::vector<QCDPFJet>                    PFJets_;
       //---- PFJetsCHS -----------------------------------------------
--- AnalysisFW/src/QCDEvent.cc
+++ AnalysisFW/src/QCDEvent.cc
@@ -12,13 +12,13 @@ QCDEvent::~QCDEvent()
 {
 }
 //---------------------------------------------------
-//void QCDEvent::setCaloJets(const std::vector<QCDCaloJet>& fCaloJets)
-//{
-//  CaloJets_.clear();
-//  for(unsigned i=0;i<fCaloJets.size();i++) {
-//    CaloJets_.push_back(fCaloJets[i]);
-//  }
-//}
+void QCDEvent::setCaloJets(const std::vector<QCDCaloJet>& fCaloJets)
+{
+ CaloJets_.clear();
+ for(unsigned i=0;i<fCaloJets.size();i++) {
+   CaloJets_.push_back(fCaloJets[i]);
+ }
+}
 //---------------------------------------------------
 void QCDEvent::setPFJets(const std::vector<QCDPFJet>& fPFJets)
 {
@@ -221,36 +221,36 @@ float QCDEvent::pfchsmjjcor(int k,int src)
 //  }
 //}
 //---------------------------------------------------
-//float QCDEvent::calomjj()
-//{
-//  if (CaloJets_.size() < 2)
-//    return 0.0;
-//  else {
-//    const LorentzVector& P0 = CaloJets_[0].p4();
-//    const LorentzVector& P1 = CaloJets_[1].p4();
-//    return (P0+P1).mass();
-//  }
-//}
+float QCDEvent::calomjj()
+{
+ if (CaloJets_.size() < 2)
+   return 0.0;
+ else {
+   const LorentzVector& P0 = CaloJets_[0].p4();
+   const LorentzVector& P1 = CaloJets_[1].p4();
+   return (P0+P1).mass();
+ }
+}
 ////---------------------------------------------------
-//float QCDEvent::calomjjcor(int k)
-//{
-//  int sign(0);
-//  if (CaloJets_.size() < 2)
-//    return 0.0;
-//  else {
-//    if (k>0)
-//      sign = 1;
-//    if (k<0)
-//      sign = -1;
-//    const LorentzVector& P0 = CaloJets_[0].p4();
-//    const LorentzVector& P1 = CaloJets_[1].p4();
-//    double cor0 = CaloJets_[0].cor();
-//    double cor1 = CaloJets_[1].cor();
-//    double unc0 = CaloJets_[0].unc();
-//    double unc1 = CaloJets_[1].unc();
-//    return (cor0*(1+sign*unc0)*P0+cor1*(1+sign*unc1)*P1).mass();
-//  }
-//}
+float QCDEvent::calomjjcor(int k)
+{
+ int sign(0);
+ if (CaloJets_.size() < 2)
+   return 0.0;
+ else {
+   if (k>0)
+     sign = 1;
+   if (k<0)
+     sign = -1;
+   const LorentzVector& P0 = CaloJets_[0].p4();
+   const LorentzVector& P1 = CaloJets_[1].p4();
+   double cor0 = CaloJets_[0].cor();
+   double cor1 = CaloJets_[1].cor();
+   double unc0 = CaloJets_[0].unc();
+   double unc1 = CaloJets_[1].unc();
+   return (cor0*(1+sign*unc0)*P0+cor1*(1+sign*unc1)*P1).mass();
+ }
+}
 //---------------------------------------------------
 float QCDEvent::pfmjjgen()
 {
@@ -263,15 +263,15 @@ float QCDEvent::pfmjjgen()
   }
 }
 //---------------------------------------------------
-//float QCDEvent::calomjjgen()
-//{
-//  if (CaloJets_.size() < 2)
-//    return 0.0;
-//  else {
-//    const LorentzVector& P0 = CaloJets_[0].genp4();
-//    const LorentzVector& P1 = CaloJets_[1].genp4();
-//    return (P0+P1).mass();
-//  }
-//}
+float QCDEvent::calomjjgen()
+{
+ if (CaloJets_.size() < 2)
+   return 0.0;
+ else {
+   const LorentzVector& P0 = CaloJets_[0].genp4();
+   const LorentzVector& P1 = CaloJets_[1].genp4();
+   return (P0+P1).mass();
+ }
+}
 
 
