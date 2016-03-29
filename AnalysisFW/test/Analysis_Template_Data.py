66feb0479a5cbdff232e3d3880b39bc291e52bbc
Paolo <paolo.gunnellini@cern.ch>
2016-03-29 14:36:14 +0200

    analyzer Engin

--- AnalysisFW/test/Analysis_Template_Data.py
+++ AnalysisFW/test/Analysis_Template_Data.py
@@ -2,7 +2,7 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("myprocess")
-process.TFileService=cms.Service("TFileService",fileName=cms.string('My_azi_data_Run2012A_test.root'))
+process.TFileService=cms.Service("TFileService",fileName=cms.string('TriggerTagAndProbe-ak4.root'))
 
 ##-------------------- Define the source  ----------------------------
 process.maxEvents = cms.untracked.PSet(
@@ -11,34 +11,40 @@ process.maxEvents = cms.untracked.PSet(
 process.source = cms.Source("EmptySource")
 
 ##-------------------- User analyzer  --------------------------------
-process.efficiency  = cms.EDAnalyzer('Analysis_Template_Data',
-
-     filename        = cms.string('/mnt/storage/Azimuthal/Data_19thNovember2014/19thNovember2014_Run2012A.root'),
+process.efficiency  = cms.EDAnalyzer('Analysis_Template_MC',
+                                     filename        = cms.vstring('','','','','root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/Data/Ntuples-Data-MagneticField-JetHt-JsonFile-25-Run2015C_v2.root','root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/Data/Ntuples-Data-MagneticField-JetHt-JsonFile-25-Run2015B_v2.root'),
+                                     #filename        = cms.vstring('','','','','root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/Data/Ntuples-Data-MagneticField-JetHt-JsonFile-25-Run2015B_v2.root'),
 
      treename        = cms.string('ProcessedTree'),
-     dirname         = cms.string('ak7'),
+     dirname         = cms.string('ak4'),
      
-     minPt           = cms.double(100.0),
-     ymax            = cms.double(2.5),
+     minPt           = cms.double(20.0),
+     ymax            = cms.double(4.7),
      JetID           = cms.int32(2),
 
      printOk         = cms.int32(1),
-
+     pseudoglobaltag = cms.string('Summer15_50nsV5'), #Make sure that the name is the same as in data direcotry                                              
+     jettype         = cms.string('AK4PFchs'),     #AKXPF or AKXPFchs  -  this is only for JECs  
+                                     
           
      hcalNoiseFilter = cms.int32(0),
      isMCarlo        = cms.untracked.bool(False),
-
-     jecUncSrcNames  = cms.vstring('AbsoluteStat','AbsoluteScale','AbsoluteFlavMap', 'AbsoluteMPFBias', 'Fragmentation', 'SinglePionECAL',
-				    'SinglePionHCAL', 'FlavorQCD', 'TimeEta', 'TimePt', 'RelativeJEREC1', 'RelativeJEREC2', 'RelativeJERHF',
-				    'RelativePtBB', 'RelativePtEC1', 'RelativePtEC2', 'RelativePtHF', 'RelativeFSR', 'RelativeStatFSR',
-				    'RelativeStatEC2', 'RelativeStatHF', 'PileUpDataMC', 'PileUpPtRef', 'PileUpPtBB', 'PileUpPtEC1', 
-				    'PileUpPtEC2', 'PileUpPtHF', 'PileUpMuZero', 'PileUpEnvelope', 
-				    'SubTotalPileUp', 'SubTotalRelative', 'SubTotalPt', 'SubTotalScale', 'SubTotalAbsolute', 'SubTotalMC', 'Total','TotalNoFlavor',
-				    'TotalNoTime','TotalNoFlavorNoTime',
-				    'FlavorZJet','FlavorPhotonJet','FlavorPureGluon','FlavorPureQuark','FlavorPureCharm',
-				    'FlavorPureBottom','TimeRunA','TimeRunB','TimeRunC','TimeRunD','CorrelationGroupMPFInSitu',
-				    'CorrelationGroupIntercalibration','CorrelationGroupbJES','CorrelationGroupFlavor',
-				    'CorrelationGroupUncorrelated'),
+     PUReweighting        = cms.untracked.bool(False),
+     LowPileUp  = cms.untracked.bool(False),                                   
+     MCSlice = cms.int32(0),                                     
+     jecUncSrc  = cms.string('../data/Summer15_50nsV5/Summer15_50nsV5_DATA_Uncertainty_AK4PFchs.txt'),#test.txt'),#Summer15_50nsV5_DATA_Uncertainty_AK8PFchs.txt'),
+                                     #jecUncSrc  = cms.string('../data/Summer15_50nsV5/test.txt'),#Summer15_50nsV5_DATA_UncertaintySources_AK8PFchs.txt'),
+     jecUncSrcNames  = cms.vstring(''),#'AbsoluteStat,AbsoluteFlavMap', 'AbsoluteMPFBias', 'Fragmentation', 'SinglePionECAL',
+                                   #'SinglePionHCAL', 'FlavorQCD', 'TimeEta', 'TimePt', 'RelativeJEREC1', 'RelativeJEREC2', 'RelativeJERHF',
+				   #'RelativePtBB', 'RelativePtEC1', 'RelativePtEC2', 'RelativePtHF', 'RelativeFSR', 'RelativeStatFSR',
+				   #'RelativeStatEC2', 'RelativeStatHF', 'PileUpDataMC', 'PileUpPtRef', 'PileUpPtBB', 'PileUpPtEC1', 
+				   #'PileUpPtEC2', 'PileUpPtHF', 'PileUpMuZero', 'PileUpEnvelope', 
+				   #'SubTotalPileUp', 'SubTotalRelative', 'SubTotalPt', 'SubTotalScale', 'SubTotalAbsolute', 'SubTotalMC', 'Total','TotalNoFlavor',
+				   #'TotalNoTime','TotalNoFlavorNoTime',
+				   #'FlavorZJet','FlavorPhotonJet','FlavorPureGluon','FlavorPureQuark','FlavorPureCharm',
+				   #'FlavorPureBottom','TimeRunA','TimeRunB','TimeRunC','TimeRunD','CorrelationGroupMPFInSitu',
+				   #'CorrelationGroupIntercalibration','CorrelationGroupbJES','CorrelationGroupFlavor',
+				   #'CorrelationGroupUncorrelated'),
 		  )
 
 process.p = cms.Path(process.efficiency)
--- AnalysisFW/test/Analysis_Template_MC.py
+++ AnalysisFW/test/Analysis_Template_MC.py
@@ -2,7 +2,7 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("myprocess")
-process.TFileService=cms.Service("TFileService",fileName=cms.string("out_Analysis_Template_MC.root"))
+process.TFileService=cms.Service("TFileService",fileName=cms.string("Pythia8-MagneticField-Sliced-AK4-Summer15_50nsV5-HighStat-ReweightRunC.root"))
 
 ##-------------------- Define the source  ----------------------------
 process.maxEvents = cms.untracked.PSet(
@@ -13,33 +13,38 @@ process.source = cms.Source("EmptySource")
 
 ##-------------------- User analyzer  --------------------------------
 process.efficiency  = cms.EDAnalyzer('Analysis_Template_MC',
-    filename        = cms.string("file://./MC_ProcessedTreeProducer_2.root"),
-
-    treename        = cms.string('ProcessedTree'),
-    dirname         = cms.string('ak7'),
+                                                     filename        = cms.vstring(
+#filename        = cms.vstring("root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8-CUETP8M1-Ntuples-PFJets.root"),
+"root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-5-10GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-10-15GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-15-30GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-30-50GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-50-80GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-80-120GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-120-170GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-170-300GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-300-470GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-470-600GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-600-800GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-800-1000GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-1000-1400GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-1400-1800GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-1800-2400GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-2400-3200GeVPtHat.root","root://eoscms//eos/cms/store/group/phys_smp/Multijet/13TeV/MC/Pythia8_CUETP8M1_PtHatSliced/Pythia8-CUETP8M1-Ntuples-3200-InfGeVPtHat.root"),
+                                     treename        = cms.string('ProcessedTree'),
+    dirname         = cms.string('ak4'),
 
     #Variables for JECs on the fly
-    pseudoglobaltag = cms.string('PHYS14_V4'), #Make sure that the name is the same as in data direcotry
-    jettype         = cms.string('AK7PF'),     #AKXPF or AKXPFchs  -  this is only for JECs
+    pseudoglobaltag = cms.string('Summer15_50nsV5'), #Make sure that the name is the same as in data direcotry
+    jettype         = cms.string('AK4PFchs'),     #AKXPF or AKXPFchs  -  this is only for JECs
     #
-
-    minPt           = cms.double(100.0),
-    ymax            = cms.double(2.5),
+    minPt           = cms.double(50.0),
+    ymax            = cms.double(4.7),
     JetID           = cms.int32(2),
 
-    printOk         = cms.int32(0),
+    printOk         = cms.int32(18),
 
     #xsection        = cms.double(10360000.0),
     #mcevents        = cms.double(52000000),
-
+    MCSlice         = cms.int32(1),                                 
+    PUReweighting   = cms.untracked.bool(True),                                 
+    LowPileUp  = cms.untracked.bool(False),                                 
     isMCarlo        = cms.untracked.bool(True),
-    jecUncSrcNames  = cms.vstring('Absolute','HighPtExtra','SinglePionECAL','SinglePionHCAL','FlavorQCD',
-                               'Time','RelativeJEREC1','RelativeJEREC2','RelativeJERHF','RelativePtBB',
-                               'RelativePtEC1','RelativePtEC2','RelativePtHF','RelativeFSR','RelativeStatEC2',
-                               'RelativeStatHF','PileUpDataMC','PileUpPtBB','PileUpPtEC','PileUpPtHF','PileUpBias',
-                               'TotalNoFlavor', 'FlavorZJet','FlavorPhotonJet','FlavorPureGluon','FlavorPureQuark',
-                               'FlavorPureCharm','FlavorPureBottom',
-                               'SubTotalPileUp','SubTotalRelative','SubTotalPt','SubTotalMC','Total'),
+                                     jecUncSrc  = cms.string('../data/Summer15_50nsV5/Summer15_50nsV5_MC_Uncertainty_AK4PFchs.txt'),    
+    #jecUncSrc  = cms.string('../data/Summer15_50nsV5/test.txt'),    
+    jecUncSrcNames  = cms.vstring(''),
+     #jecUncSrcNames  = cms.vstring('Absolute','HighPtExtra','SinglePionECAL','SinglePionHCAL','FlavorQCD',
+                               #'Time','RelativeJEREC1','RelativeJEREC2','RelativeJERHF','RelativePtBB',
+                               #'RelativePtEC1','RelativePtEC2','RelativePtHF','RelativeFSR','RelativeStatEC2',
+                               #'RelativeStatHF','PileUpDataMC','PileUpPtBB','PileUpPtEC','PileUpPtHF','PileUpBias',
+                               #'TotalNoFlavor', 'FlavorZJet','FlavorPhotonJet','FlavorPureGluon','FlavorPureQuark',
+                               #'FlavorPureCharm','FlavorPureBottom',
+                               #'SubTotalPileUp','SubTotalRelative','SubTotalPt','SubTotalMC','Total'),
 
 )
 
